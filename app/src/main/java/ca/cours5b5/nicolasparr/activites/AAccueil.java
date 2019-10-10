b719c8b460328231cc29a94c9e525cd8161e52a3
AuKain <nico.parr@hotmail.com>
2019-10-09 20:49:49 -0400

    Application démarre sur la page Accueil et les boutons Settings et Play fonctionnent et amènent aux activités appropriées Des appels GLog on aussi été ajoutés sur tous les états du cycle de vie de lapplication

--- app/src/main/java/ca/cours5b5/nicolasparr/activites/AAccueil.java
+++ app/src/main/java/ca/cours5b5/nicolasparr/activites/AAccueil.java
@@ -1,5 +1,8 @@
 package ca.cours5b5.nicolasparr.activites;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import ca.cours5b5.nicolasparr.R;
@@ -21,6 +24,22 @@ public class AAccueil extends ActiviteAvecControles {
         buttonPlay = findViewById(R.id.buttonPlay);
         buttonOnline = findViewById(R.id.buttonOnline);
         buttonSettings = findViewById(R.id.buttonSettings);
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(AAccueil.this, APartieLocale.class));
+            }
+        });
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(AAccueil.this, AParametres.class));
+            }
+        });
 
         GLog.valeurs(buttonConnection, buttonPlay, buttonOnline, buttonSettings);
     }
--- app/src/main/java/ca/cours5b5/nicolasparr/activites/Activite.java
+++ app/src/main/java/ca/cours5b5/nicolasparr/activites/Activite.java
@@ -1,8 +1,12 @@
 package ca.cours5b5.nicolasparr.activites;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import ca.cours5b5.nicolasparr.global.GLog;
 
 public abstract class Activite extends AppCompatActivity {
 
@@ -13,6 +17,57 @@ public abstract class Activite extends AppCompatActivity {
         int contentViewId = getLayoutId();
 
         setContentView(contentViewId);
+
+        GLog.valeurs(this);
+        GLog.valeurs("état, " + savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GLog.valeurs(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GLog.valeurs(this);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        GLog.valeurs(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        GLog.valeurs(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        GLog.valeurs(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        GLog.valeurs(this);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        GLog.valeurs(this);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        GLog.valeurs(this);
     }
 
     protected abstract int getLayoutId();
